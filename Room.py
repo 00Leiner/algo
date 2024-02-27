75dbe55ce6ace0c9358c1252bab10eb7852ac350
Leiner <leinersacdalan4@gmail.com>
2024-02-27 13:41:36 +0000

    entry for room constraints

--- Room.py
+++ Room.py
@@ -1,9 +1,10 @@
 class Room:
   def __init__(self, room, day, time):
     self.room = room
-    self.room_capacity = {}
+    self.room_capacity = {} #make sure no overlapping schedule [room_id, day, time]
 
   def room_constraints(self, student, type, day, time):
     pass
+
 
   
